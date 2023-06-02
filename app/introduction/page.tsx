4387d863a415a4f10d0ef292603ebc13926bec9c
Heaven Chen <heavenchen@qnap.com>
2023-06-03 03:27:30 +0800

    Add introduction page

---
+++ app/introduction/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Introduction() {
+  return (
+    <main className="flex flex-col justify-start items-center min-h-screen pt-40 mx-10 gap-11 md:mx-32 md:gap-16">
+      <h1 className="uppercase tracking-widest font-black text-red-600 text-4xl md:text-5xl">
+        introduction
+      </h1>
+      <div className="h-full w-full outline outline-offset-4 bg-zinc-900 outline-zinc-900 text-justify text-white rounded-md text-lg p-9 leading-10 mb-20 md:text-2xl md:p-14 md:leading-[50px]">
+        <p>
+          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+          cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, officia
+          excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem
+          pariatur mollit ex esse exercitation amet. Nisi anim cupidatat
+          excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est
+          aliquip amet voluptate voluptate dolor minim nulla est proident.
+          Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt
+          ex occaecat reprehenderit commodo officia dolor Lorem duis laboris
+          cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi
+          laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit
+          commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint
+          cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+        </p>
+      </div>
+    </main>
+  );
+}
