2ed3551563ec7c94b8896c6a4c472d482e34d2e4
Heaven Chen <heavenchen@qnap.com>
2023-06-04 03:37:51 +0800

    Change footer layout to cater to RWD design

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,7 +7,6 @@ export default function Footer() {
   return (
     <footer className="flex flex-col gap-6 uppercase items-center justify-center w-full py-4 bg-zinc-900 text-white outline outline-zinc-900 outline-offset-4 outline-2">
       <div className="inline-flex gap-1 justify-center items-center hover:text-red-600 text-zinc-900 cursor-pointer">
-        <BiRightArrow />
         <a className="text-4xl text-white" href="https://github.com/HeavenAQ">
           <IoLogoGithub />
         </a>
@@ -26,9 +25,9 @@ export default function Footer() {
         })}
       </div>
       <div className="flex flex-col gap-4 items-center justify-center">
-        <p className="text-gray-400 text-[9px] md:text-xs">
-          This is a project for NSYSU DFLL 2023 Spring English Literature. It is
-          not for commercial use.
+        <p className="text-gray-400 text-[9px] md:text-xs text-center">
+          This is a project for NSYSU DFLL 2023 Spring English Literature.
+          <br /> It is not for commercial use.
         </p>
         <p className="text-gray-400 text-[9px] md:text-xs">
           &copy; 2023 Yi-Min Chen. All Rights Reserved.
